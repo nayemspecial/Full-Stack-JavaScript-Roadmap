b1ca046896853f8a30a6ecfd0d25884e959aed8f
nayemspecial <nayemspecial@gmail.com>
2026-02-28 23:23:37 +0600

    1.6 Variable code  Added

---
+++ Course-Materials/Source-Codes/01. Introduction/1.6 Variable/variable.js
@@ -0,0 +1,9 @@
+// 1word, 9people
+// a-z, A-Z, $, _ (name, Name, $name, _name)
+// name, Name (case sensitive)
+// myName, productImage, bestProductPrice
+// let, var, const, for, while, 
+
+let name = "Nayem";
+name = "Polash Mahmud";
+console.log(name);

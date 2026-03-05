f1430bcf541fccd7ed3643c600f7d24cc3787d85
nayemspecial <nayemspecial@gmail.com>
2026-03-05 23:54:55 +0600

    2.4 String Method added

---
+++ Course-Materials/Source-Codes/02. Data Types/2.4 String Method/stringMethod.js
@@ -0,0 +1,19 @@
+let js = "JavaScript is a Programming Language.";
+console.log( js.length);
+console.log( js[16]);
+console.log( js.charAt(16));
+console.log( js.substr(4,6));
+console.log( js.substring(4,10));
+console.log( js.toUpperCase());
+console.log( js.toLocaleLowerCase());
+console.log( js.split(" "));
+console.log( js.trim());
+console.log( js.includes("Programming")); //Boolean
+console.log( js.search("Programming")); // Index
+console.log( js.replace("JavaScript", "PHP"));
+console.log( js.charCodeAt(1));
+console.log( js.indexOf("P"));
+console.log( js.concat('JavaScript ', 'is ', 'a ', 'Programming ', 'Language.'));
+console.log( js.repeat(3));
+
+

7ea12f9ad6cbf71f37c229900540df529892aeb5
nayemspecial <nayemspecial@gmail.com>
2026-03-10 22:42:23 +0600

    3.1 arithmatic operators added

---
+++ Course-Materials/Source-Codes/03. Operators/3.1 Arithmetic/arithmatic.js
@@ -0,0 +1,18 @@
+/*
+Operator
+Operand (a + b)
+*/
+let numOne = 12;
+let numTwo = 3;
+let sum = numOne + numTwo;
+let sub = numOne - numTwo;
+let product = numOne * numTwo;
+let div = numOne / numTwo;
+let reminder = numOne % numTwo;
+let powerOf = numOne ** numTwo;
+console.log(sum);
+console.log(sub);
+console.log(product);
+console.log(div);
+console.log(reminder);
+console.log(powerOf);

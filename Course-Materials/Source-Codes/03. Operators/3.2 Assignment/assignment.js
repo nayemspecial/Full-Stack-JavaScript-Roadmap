287f90aa83a954dbd816ac27ef6a239a1731b0bb
nayemspecial <nayemspecial@gmail.com>
2026-03-11 21:58:11 +0600

    3.1 assignment operators added

---
+++ Course-Materials/Source-Codes/03. Operators/3.2 Assignment/assignment.js
@@ -0,0 +1,32 @@
+/*
+= 
++=
+-=
+*=
+/=
+%=
+*/
+
+let x = 10;
+let y = 4;
+x += 3;   // x = x + 3;
+console.log(x);
+x -= 3;   // x = x - 3;
+console.log(x);
+x *= 3;   // x = x * 3;
+console.log(x);
+x /= 3;   // x = x / 3;
+console.log(x);
+x %= 3;   // x = x % 3;
+console.log(x);
+
+x += y;   // x = x + y;
+console.log(x);
+x -= y;   // x = x - y;
+console.log(x);
+x *= y;   // x = x * y;
+console.log(x);
+x /= y;   // x = x / y;
+console.log(x);
+x %= y;   // x = x % y;
+console.log(x);

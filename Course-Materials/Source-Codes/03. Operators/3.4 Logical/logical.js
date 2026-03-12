12af3a989f5c3e811f4edf2211ee1c20b1fdb39f
nayemspecial <nayemspecial@gmail.com>
2026-03-12 10:24:45 +0600

    3.4 Logical Operator Added

---
+++ Course-Materials/Source-Codes/03. Operators/3.4 Logical/logical.js
@@ -0,0 +1,28 @@
+// Logical And(&&)
+console.log(true && true);
+console.log(true && false);
+console.log(false && true);
+console.log(false && false);
+console.log(1 && 1);
+
+// Logical OR (||)
+
+console.log(true || true);
+console.log(true || false);
+console.log(false || true);
+console.log(false || false);
+
+// Logical Not (!)
+
+console.log(!false);
+console.log(!true);
+
+console.log(4 > 3 && 10 > 5);
+console.log(4 < 3 && 10 > 5);
+console.log(4 > 3 && 10 < 5);
+console.log(4 < 3 && 10 < 5);
+
+console.log(4 > 3 || 10 > 5);
+console.log(4 < 3 || 10 > 5);
+console.log(4 > 3 || 10 < 5);
+console.log(4 < 3 || 10 < 5);

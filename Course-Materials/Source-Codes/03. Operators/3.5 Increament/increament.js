2462692fd03d6ba029f3128e16f3af1c3fdfad92
nayemspecial <nayemspecial@gmail.com>
2026-03-14 22:55:27 +0600

    3.5 Inc Dec Operator Added

---
+++ Course-Materials/Source-Codes/03. Operators/3.5 Increament/increament.js
@@ -0,0 +1,27 @@
+let a = 4;
+a++; // Increament
+a--; // Decreament
+
+a = a + 1;
+console.log(a);
+a = a - 1;
+console.log(a);
+
+a = 4;
+b = a++;
+console.log(b);
+console.log(a);
+
+a = 4;
+b = ++a;
+console.log(b);
+console.log(a);
+
+
+
+
+
+
+
+
+

56c88143c18a5a84872a91e3e3d67c5f34f21348
nayemspecial <nayemspecial@gmail.com>
2026-03-15 23:52:41 +0600

    4.1 If conditionals Added

---
+++ Course-Materials/Source-Codes/04. Conditionals/4.1 If Else/if else.js
@@ -0,0 +1,10 @@
+let num = "Nayem";
+if ( num > 0 ) {
+    console.log(`${num} is a positive number.`);
+} else if ( num < 0 ) {
+    console.log(`${num} is a negative number.`);
+} else if ( num == 0 ) {
+    console.log(`${num}. It is zero`);
+} else {
+    console.log(`${num}. Is not a number.`);
+}

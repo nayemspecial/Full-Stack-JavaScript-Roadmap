56f1a5c4acbbd8420eef78e669474f9ec133566d
nayemspecial <nayemspecial@gmail.com>
2026-03-17 23:10:57 +0600

    4.3 Else if logical operator Added

---
+++ Course-Materials/Source-Codes/04. Conditionals/4.3 else if and logical operator/maxOfThreeNum.js
@@ -0,0 +1,8 @@
+let a =100, b = 200, c = 30;
+if ( a>b && a>c ) {
+    console.log(`${a} is the maximum number.`);
+} else if ( b>a && b>c) {
+    console.log(`${b} is the maximum number.`);
+} else {
+    console.log(`${c} is the maximum number.`);
+}

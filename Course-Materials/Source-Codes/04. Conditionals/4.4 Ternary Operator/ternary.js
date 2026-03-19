b1cda3298d6180a0adfaa80f34421b8cff2a9035
nayemspecial <nayemspecial@gmail.com>
2026-03-19 11:52:58 +0600

    4.4 Ternary Operator Added

---
+++ Course-Materials/Source-Codes/04. Conditionals/4.4 Ternary Operator/ternary.js
@@ -0,0 +1,12 @@
+let marks = 80;
+if ( marks >= 40 ) {
+    console.log("You are passed.");
+} else {
+    console.log("You are Failed.");
+}
+
+marks >= 40 
+? console.log("You are passed.") 
+: console.log("You are Failed.") ;
+
+// condition ? true : false;
